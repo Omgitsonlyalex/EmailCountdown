8f56f9aab2d051fb562ab56c671dec94a021abe9
Stev Leibelt <stev@leibelt.de>
2015-11-10 15:51:41 +0100

    Fixed issue by using font path to a not existing file

--- gif.php
+++ gif.php
@@ -18,12 +18,12 @@
 	$delay = 100;// milliseconds
 
 	$font = array(
-		'size'=>23, // Font size, in pts usually.
-		'angle'=>0, // Angle of the text
-		'x-offset'=>7, // The larger the number the further the distance from the left hand side, 0 to align to the left.
-		'y-offset'=>30, // The vertical alignment, trial and error between 20 and 60.
-		'file'=>'./GillSans.ttc', // Font path
-		'color'=>imagecolorallocate($image, 55, 160, 130), // RGB Colour of the text
+		'size' => 23, // Font size, in pts usually.
+		'angle' => 0, // Angle of the text
+		'x-offset' => 7, // The larger the number the further the distance from the left hand side, 0 to align to the left.
+		'y-offset' => 30, // The vertical alignment, trial and error between 20 and 60.
+		'file' => __DIR__ . DIRECTORY_SEPARATOR . 'Futura.ttc', // Font path
+		'color' => imagecolorallocate($image, 55, 160, 130), // RGB Colour of the text
 	);
 	for($i = 0; $i <= 60; $i++){
 		
